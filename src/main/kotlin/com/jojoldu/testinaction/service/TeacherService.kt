0d21d51506fde61d33544956b795b4f42b0057dd
jojoldu <jojoldu@gmail.com>
2023-11-25 22:50:31 +0900

    예제코드 1,2,3 추가

--- src/main/kotlin/com/jojoldu/testinaction/service/TeacherService.kt
+++ src/main/kotlin/com/jojoldu/testinaction/service/TeacherService.kt
@@ -1,5 +1,6 @@
-package com.jojoldu.testinaction
+package com.jojoldu.testinaction.service
 
+import com.jojoldu.testinaction.entity.teacher.Teacher
 import com.jojoldu.testinaction.entity.teacher.TeacherRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,5 +11,10 @@ class TeacherService (private val teacherRepository: TeacherRepository){
 
     fun countStudents (teacherId : Long) : Int {
         return teacherRepository.findById(teacherId).get().countStudents()
+    }
+
+    @Transactional
+    fun saveAll (teachers: List<Teacher>) : Int {
+        return teacherRepository.saveAll(teachers).size
     }
 }
---
+++ src/test/kotlin/com/jojoldu/testinaction/service/OriginalTest1.kt
@@ -0,0 +1,39 @@
+package com.jojoldu.testinaction.service
+
+import com.jojoldu.testinaction.entity.teacher.Student
+import com.jojoldu.testinaction.entity.teacher.Teacher
+import com.jojoldu.testinaction.entity.teacher.TeacherRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OriginalTest1 {
+
+    @Autowired
+    private lateinit var teacherService: NoTxTeacherService
+
+    @Autowired
+    private lateinit var teacherRepository: TeacherRepository
+
+    @AfterEach
+    fun tearDown() {
+        teacherRepository.deleteAll()
+    }
+
+    @Test
+    fun `teacher의 students count를 조회할 수 있다`() {
+        val email = "jojoldu@gmail.com"
+        val teacher = Teacher(name = "jojoldu", email = email)
+        teacher.addStudent(Student(name = "John", email = "John@gmail.com", teacher = teacher))
+        teacher.addStudent(Student(name = "Jane", email = "Jane@gmail.com", teacher = teacher))
+
+        teacherRepository.save(teacher)
+
+        val result = teacherService.countStudents(teacher.id!!)
+
+        assertThat(result).isEqualTo(2)
+    }
+}
